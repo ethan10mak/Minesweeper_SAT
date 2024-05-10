5f3d9971715d9603f30e24f91edf31eabb4014c6
ethan10mak <ethan10mak@gmail.com>
2024-05-09 17:27:10 -0700

    Created board.py and sat_solver.py with basic functionality.

---
+++ board.py
@@ -0,0 +1,56 @@
+# The game board representing Minesweeper
+# Responsible for outputting the current state of the board and setting up the game board.
+# Installed pycryptosat
+# Run line pip install pycryptosat
+import numpy as np
+from sat_solver import sat_solve
+
+rows = 3
+columns = 3
+dim = rows * columns
+
+
+def get_board_position(board, i, j):
+    return board[i][j]
+
+
+def print_board(board):
+    for i in board:
+        for j in i:
+            if j == 9:
+                print(" *", end="")
+            else:
+                print("", j, end="")
+        print("")
+    return 0
+
+
+# Generate Mine board function
+def mine_board():
+    return [[9, 10, 10], [10, 10, 10], [10, 10, 9]]
+
+
+# Generate Numbers in different board function
+# Given mine board, give numbers that represent how many mines are next to it
+# The result is the final solution of the board
+def generate_numbers(board):
+    return [[mine, 1, 0], [1, 2, 1], [0, 1, mine]]
+
+
+# solve_current_state
+def solve_current_state(current_board, numbers_board):
+    sat_solve.solve(sat_solve, current_board)
+    return 0
+
+
+# States: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+# 0 = No mines next to it
+# 9 = mine
+# 10 = Not Discovered
+
+mine = 9
+undiscovered = -1
+board = [[mine, 1, 0], [1, 2, 1], [0, 1, mine]]
+current_board = [[10, 10, 10], [10, 10, 10], [0, 10, 10]]
+numbers_board = generate_numbers(mine_board)
+solve_current_state(current_board, numbers_board)
---
+++ sat_solver.py
@@ -0,0 +1,54 @@
+# File for solving the minesweeper problem
+import sys
+
+from pycryptosat import Solver
+import numpy as np
+
+rows = 3
+columns = 3
+dim = rows * columns
+mine = 9
+k_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+class sat_solve:
+    def __init__(self):
+        self.rows = 3
+        self.columns = 3
+        self.dim = self.rows * self.columns
+        self.mine = 9
+        self.k_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    # Returns the corresponding variable number
+    # Ex var(i, j, k) represents variable x at row i, column j, and value k
+
+    def var(i, j, k):
+        return (i - 1) * rows * columns + (j - 1) * columns + (k - 1) + 1
+
+    def check_squares_around(self, board, i, j):
+        return
+
+    def solve(self, board):
+        # k_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        clauses = []
+        for i in range(1, rows + 1):
+            for j in range(1, columns + 1):
+                # C1 the entry at row i col j has at least one value
+                clauses.append([self.var(i, j, k) for k in k_values])
+
+                # C2 the entry at row i col j has at most on value
+                for k in range(1, rows + 1):
+                    for l in range(k + 1, columns + 1):
+                        clauses.append([-self.var(i, j, k), -self.var(i, j, k)])
+
+        # for i in range(1, rows + 1):
+        #    for j in range(1, columns + 1):
+        # Blan
+
+        s = Solver()
+        for clause in clauses:
+            print(clause)
+            s.add_clause(clause)
+        sat, solution = s.solve()
+        print(sat)
+        print(solution)
